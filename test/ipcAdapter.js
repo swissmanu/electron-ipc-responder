a009985c1f042cfb6105fb4d7d4cd73944b3e54b
Manuel Alabor <manuel@alabor.me>
2016-01-08 10:29:36 +0100

    test(ipcadapter): Add some more tests (simple ones 😇)
    Contributes to #1

--- test/ipcAdapter.js
+++ test/ipcAdapter.js
@@ -13,5 +13,23 @@ describe('IPCAdapter', () => {
     it('should keep given send function inside the IPCAdapter', () => {
       expect(this.ipcAdapter.send).toEqual(this.send)
     })
+
+    it('should initialize topicHandlers with an empty object', () => {
+      expect(this.ipcAdapter.topicHandlers).toEqual({})
+    })
+
+    it('should initialize awaitingResponseHandlers with an empty object', () => {
+      expect(this.ipcAdapter.awaitingResponseHandlers).toEqual({})
+    })
+  })
+
+  describe('registerTopic()', () => {
+    it('should set given topic handler for given topic to topicHandlers', () => {
+      const topic = 'leia'
+      const handler = 'han'
+      this.ipcAdapter.registerTopic(topic, handler)
+
+      expect(this.ipcAdapter.topicHandlers[topic]).toEqual(handler)
+    })
   })
 })
